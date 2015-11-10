0180ade15709991bd3a8df6e9c1e3e52a2b81c17
Beckylee <dr.oreo@gmail.com>
2015-11-10 17:24:22 -0500

    added recipe page with links

---
+++ browser/js/recipes/recipes.js
@@ -0,0 +1,38 @@
+app.config(function ($stateProvider) {
+    $stateProvider.state('recipes', {
+        url: '/recipes',
+        templateUrl: 'js/recipes/recipes.html',
+        controller: 'recipesCtrl'
+    });
+});
+
+app.controller('recipesCtrl', function($scope){
+  $scope.apps = [
+    { name: 'Spinach Artichoke Dip', link: 'http://www.foodnetwork.com/recipes/alton-brown/hot-spinach-and-artichoke-dip-recipe.html'},
+    { name: 'Bruschetta', link: 'http://www.simplyrecipes.com/recipes/bruschetta_with_tomato_and_basil/%22'},
+    { name: 'Kale and Squash Salad', link: 'http://thepioneerwoman.com/cooking/2014/11/butternut-squash-and-kale/'},
+    { name: 'Fresh Tomato and Corn Salad', link: 'http://www.foodnetwork.com/recipes/food-network-kitchens/fresh-corn-tomato-salad-recipe.html'}
+  ];
+
+  $scope.sides = [
+    { name: 'Sweet Potato Fries', link: 'http://www.foodnetwork.com/recipes/food-network-kitchens/fresh-corn-tomato-salad-recipe.html'},
+    { name: 'Chicken Salad (my own recipe)', link: ''},
+    { name: 'Brussel Sprouts', link: 'http://www.foodnetwork.com/recipes/food-network-kitchens/fresh-corn-tomato-salad-recipe.html%22'},
+    { name: 'All vegetables, in general', link: ''}
+  ];
+
+  $scope.mains = [
+    { name: 'Chicken Marbella', link: 'http://www.simplyrecipes.com/recipes/chicken_marbella/'},
+    { name: 'Chicken Marsala with Sage', link: 'http://www.bonappetit.com/recipe/chicken-marsala-with-sage'},
+    { name: 'Eggplant Parm', link: 'http://www.foodnetwork.com/recipes/mario-batali/eggplant-parmigiana-recipe.html'},
+    { name: 'Beef Stew', link: 'http://thepioneerwoman.com/cooking/sunday-night-stew/'},
+    { name: 'Chicken Noodle Soup', link: ''}
+  ];
+
+  $scope.desserts = [
+    { name: 'Chocolate Meringues', link: 'http://allrecipes.com/recipe/10751/chocolate-meringue-cookies/'},
+    { name: 'Flourless Chocolate Cake', link: 'http://www.chow.com/recipes/14636-1-favorite-chocolate-flourless-cake'},
+    { name: 'Classic Chocolate Chip Cookies', link: ''},
+    { name: 'Black Bottom Cupcakes', link: 'http://allrecipes.com/recipe/15503/grandma-gudgels-black-bottom-cupcakes/'}
+  ];
+});
