7fd927765085f4d2a87278cce8399942f04986e7
Beckylee <dr.oreo@gmail.com>
2015-11-11 16:17:17 -0500

    projects mobile friendly

--- browser/js/projects/projects.js
+++ browser/js/projects/projects.js
@@ -9,7 +9,7 @@ app.config(function ($stateProvider) {
 app.controller('projectsCtrl', function($scope){
   $scope.projects = [
     { name: 'SpellBook', link: 'http://www.myspellbook.org', blurb: 'An interactive story book that teaches children basic programming logic', color: 'purple1'},
-    { name: 'Dance Party', link: 'https://www.dance-party.herokuapp.com', blurb: 'An interactive dance party with some of your favorite Fullstack Academy characters', color:'green1'},
+    { name: 'Dance Party', link: 'https://dance-party.herokuapp.com/', blurb: 'An interactive dance party with some of your favorite Fullstack Academy characters', color:'green1'},
     { name: 'Global Citizen Festival', link: '', blurb: 'Action journey that encouraged users to take action to end extreme poverty', color:'pink1'}
   ];
 });
