d13f18853d6b42418cc4a38b7665963ee956937c
Beckylee <dr.oreo@gmail.com>
2015-11-30 15:45:04 -0500

    added static ip

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 'use strict';
 var chalk = require('chalk');
 var app = require('./app');
-var PORT = process.env.PORT || 1337;
+var PORT = process.env.QUOTAGUARDSTATIC_URL || 1337;
 
 var server = require('http').createServer(app).listen(PORT);
