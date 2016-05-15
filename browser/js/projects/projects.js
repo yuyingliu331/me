fed2c758519d4bcb82f2b644bf0df44ac366d8b2
Beckylee <dr.oreo@gmail.com>
2016-05-15 19:02:09 -0400

    added coned project to projects

--- browser/js/projects/projects.js
+++ browser/js/projects/projects.js
@@ -8,8 +8,10 @@ app.config(function ($stateProvider) {
 
 app.controller('projectsCtrl', function($scope){
   $scope.projects = [
+    { name: 'ConEd Smart AC Signup', link: 'https://conedsmartac.com/', blurb: 'A microsite that allows Con Edison customers to sign up for the Smart AC program', color: 'yellow2'},
     { name: 'SpellBook', link: 'http://www.myspellbook.org', blurb: 'An interactive story book that teaches children basic programming logic', color: 'purple1'},
     { name: 'Dance Party', link: 'https://dance-party.herokuapp.com/', blurb: 'An interactive dance party with some of your favorite Fullstack Academy characters', color:'green1'},
+    { hide: true},
     { name: 'Global Citizen Festival', link: '/gcf', blurb: 'Action journey that encouraged users to take action to end extreme poverty', color:'pink1'}
   ];
 });
