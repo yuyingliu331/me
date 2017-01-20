82c55b308bfb9d7bbe904c0af103f028c0a49670
yuyingliu331 <liuyuying90@live.cn>
2017-01-20 11:32:39 -0500

    updated home page

--- browser/js/common/constants/menu.constants.js
+++ browser/js/common/constants/menu.constants.js
@@ -1,7 +1,7 @@
 app.constant('MENU', [
   { label: 'Home', state: 'home'},
 	{ label: 'About', state: 'about'},
-	{ label: 'Travel_Places', state: 'projects'},
+	{ label: 'Travel Places', state: 'projects'},
 	{ label: 'My Furry Companion', state: 'resume'}
 
 ]);
