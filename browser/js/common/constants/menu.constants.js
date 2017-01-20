43a7afe2e128af86003134775ec8474762fe5e92
yuyingliu331 <liuyuying90@live.cn>
2017-01-19 21:37:47 -0500

    updated menu bar with about page

--- browser/js/common/constants/menu.constants.js
+++ browser/js/common/constants/menu.constants.js
@@ -2,8 +2,6 @@ app.constant('MENU', [
   { label: 'Home', state: 'home'},
 	{ label: 'About', state: 'about'},
 	{ label: 'Travel_Places', state: 'projects'},
-	{ label: 'Education Background', state: 'resume'},
-	{ label: 'Skills', state: 'skills'},
-	{ label: 'Professional Experiences', state: 'recipes'},
-	{ label: 'Contact', state: 'contact'}
+	{ label: 'My Furry Companion', state: 'resume'}
+
 ]);
